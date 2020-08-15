9b10ebc984666d972a68036ee5c6140932e44489
ghigbie <gehigbie@gmail.com>
2020-08-15 15:10:24 -0700

    Add meals navigator

--- App.js
+++ App.js
@@ -5,17 +5,22 @@ import CategoryMealsScreen from './src/screens/CategoryMealsScreen';
 import FavoritesScreen from './src/screens/FavoritesScreen';
 import FiltersScreen from './src/screens/FiltersScreen';
 import MealDetailScreen from './src/screens/MealDetailScreen';
+import MealsNavigator from './src/navigation/MealsNavigator';
 
 const App = () => {
-  return (
-    <View style={styles.appContainer}>
-      <CategoriesScreen />
-      <CategoryMealsScreen />
-      <FavoritesScreen />
-      <FiltersScreen />
-      <MealDetailScreen />
-    </View>
-  );
+  if (true) {
+    return <MealsNavigator />;
+  } else {
+    return (
+      <View style={styles.appContainer}>
+        <CategoriesScreen />
+        <CategoryMealsScreen />
+        <FavoritesScreen />
+        <FiltersScreen />
+        <MealDetailScreen />
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -1,8 +1,8 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation-stack';
 
-import CategoriesScreen from './../screens/CategoriesScreen';
-import CategoryMealsScreen from './../screens/CategoryMealsScreen';
-import MealDetailScreen from './../screens/MealDetailScreen';
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailScreen from '../screens/MealDetailScreen';
 
 const MealsNavigator = createStackNavigator({
   Categories: CategoriesScreen,
