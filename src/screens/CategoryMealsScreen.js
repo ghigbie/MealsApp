421a6b6352eed92d798ecf5e928fe0c374383f42
ghigbie <gehigbie@gmail.com>
2020-08-17 18:18:07 -0700

    Add platform styles

--- src/screens/CategoryMealsScreen.js
+++ src/screens/CategoryMealsScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Platform} from 'react-native';
 
 import CATEGORIES from './../../data/dummy-data';
+import Colors from '../../constants/Colors';
 
 const CategoryMealsScreen = ({navigation}) => {
   const catId = navigation.getParam('categoryId');
@@ -12,6 +13,17 @@ const CategoryMealsScreen = ({navigation}) => {
       <Text>{selectedCategory.title}</Text>
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = {
+  headerTitle: 'Moo',
+  headerStyle: {
+    ...Platform.select({
+      android: {backgroundColor: Colors.primaryColor},
+      ios: {backgroundColor: '#fff'},
+    }),
+  },
+  headerTintColor: Platform.OS === 'android' ? '#fff' : '#000',
 };
 
 const styles = StyleSheet.create({
