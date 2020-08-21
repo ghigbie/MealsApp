87e8abe4ca7295a11cb6bbb81956473858fa64d4
ghigbie <gehigbie@gmail.com>
2020-08-20 19:01:51 -0700

    Install new package

--- src/screens/MealDetailScreen.js
+++ src/screens/MealDetailScreen.js
@@ -2,13 +2,22 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import Colors from './../../constants/Colors';
+import {MEALS} from './../../data/dummy-data';
 
-const MealDetailScreen = () => {
+const MealDetailScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text>Meal Detail Screen</Text>
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title,
+  };
 };
 
 const styles = StyleSheet.create({
