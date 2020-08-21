e219fb04a4f74f8eac77f7175130b3690e6498cb
ghigbie <gehigbie@gmail.com>
2020-08-20 19:08:42 -0700

    Add custom header button

---
+++ src/components/CustomHeaderButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {HeaderButton} from 'react-navigation-header-buttons';
+import Colors from '../../constants/Colors';
+
+const CustomHeaderButton = () => {
+  return <HeaderButton />;
+};
+
+export default CustomHeaderButton;
