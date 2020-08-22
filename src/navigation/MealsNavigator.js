54e86c62230ee83b9b34f521024beaddd277ec4e
ghigbie <gehigbie@gmail.com>
2020-08-22 13:27:18 -0700

    Add react navigation tabs

--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -1,6 +1,7 @@
 import {Platform} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import Colors from './../../constants/Colors';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
